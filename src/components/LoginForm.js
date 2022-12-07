a5518acce08d0104b004fc16a89273476394a485
iaderegg <iadergg@gmail.com>
2022-12-07 11:07:12 -0500

    Simple login form

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,6 +1,13 @@
 import React from "react";
-import { TextInput, Button, Text } from "react-native";
+import { TextInput, Button, Text, View } from "react-native";
 
 export default function LoginForm() {
-  return <Text>Componente LoginForm</Text>;
+  return (
+    <View>
+      <Text>My Login Form</Text>
+      <TextInput placeholder="Email" />
+      <TextInput placeholder="Password" />
+      <Button title="LogIn" onPress={() => console.log("Sending...")} />
+    </View>
+  );
 }
